f0301fac4c526b70c689d2b090644e2f15e4e8ac
MaestrulPortocala <105449616+MaestrulPortocala@users.noreply.github.com>
2026-05-29 21:06:41 +0300

    Add files via upload

---
+++ MortalKombat-main/Proiect2/Clase/Sablon.java
@@ -0,0 +1,23 @@
+package Proiect2.Clase;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Sablon extends JFrame implements ActionListener {
+    public Sablon() {
+        ImageIcon image = new ImageIcon("MortalKombat-main/Proiect2/Poze/download.jpg");
+        setIconImage(image.getImage());
+        setTitle("MORTAL KOMBAT DUMBRAVIOARA EDITION");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize);
+        setLocation(0, 0);
+        setUndecorated(true);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+Cum sa rulati:
+
+
+1)Deschideti IntelliJ Idea
+2)Deschideti meniul stanga sus si selectati "Open"
+3)Alegeti programul MortalKombat-main si selectati "ok"
+4)Have fun! E un joc care trebuie jucat in 2.
